cdac92628352eb0118ff7a1e834af64721f56b06
Natalia Taira <nataliagtaira@gmail.com>
2025-07-10 13:21:39 -0300

    Insere dependência de @Module

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -4,9 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioController } from './controllers/usuario.controller';
 import { Usuario } from './entities/usuario.entity';
 import { UsuarioService } from './services/usuario.service';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario]),// forwardRef(() => AuthModule)
+  imports: [
+    TypeOrmModule.forFeature([Usuario]),
+    // forwardRef(() => AuthModule)
   ],
   providers: [UsuarioService],
   controllers: [UsuarioController],
