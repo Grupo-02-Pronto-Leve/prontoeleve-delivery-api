f0a7a0fc3c452c8d9bbb05bb42b7c603a5e18da9
taismichely <michely2207@gmail.com>
2025-07-10 16:12:40 -0300

    feat: Adicionando entity, module, controller e service de Produto

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,1 +1,38 @@
-export class Produto {}
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Categoria } from '../../categoria/entities/categoria.entity';
+import { Usuario } from './../../usuario/entities/usuario.entity';
+
+@Entity({ name: 'tb_produto' })
+export class Produto {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  nome: string;
+
+  @IsNotEmpty()
+  @Column({ length: 1000, nullable: false })
+  descricao: string;
+
+  @IsNotEmpty()
+  @Column('decimal', { precision: 10, scale: 2 })
+  preco: number;
+
+  @Column({ type: 'boolean', default: true })
+  disponivel: boolean;
+
+  @Column({ length: 255, nullable: true })
+  foto: string;
+
+  @ManyToOne(() => Categoria, (categoria) => categoria.produto, {
+    onDelete: 'CASCADE',
+  })
+  categoria: Categoria;
+
+  @ManyToOne(() => Usuario, (usuario) => usuario.produto, {
+    onDelete: 'CASCADE',
+  })
+  usuario: Usuario;
+}
--- src/produto/services/produto.service.ts
+++ src/produto/services/produto.service.ts
@@ -1,1 +1,135 @@
-export class ProdutoService {}
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, ILike, Repository } from 'typeorm';
+import { CategoriaService } from '../../categoria/services/categoria.service';
+import { UsuarioService } from '../../usuario/services/usuario.service';
+import { Produto } from '../entities/produto.entity';
+
+@Injectable()
+export class ProdutoService {
+  constructor(
+    @InjectRepository(Produto)
+    private produtoRepository: Repository<Produto>,
+    private readonly categoriaService: CategoriaService,
+    private readonly usuarioService: UsuarioService,
+  ) {}
+
+  async findAll(): Promise<Produto[]> {
+    return await this.produtoRepository.find({
+      relations: {
+        usuario: true,
+        categoria: true,
+      },
+    });
+  }
+  async findById(id: number): Promise<Produto> {
+    const produto = await this.produtoRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        usuario: true,
+        categoria: true,
+      },
+    });
+
+    if (!produto) {
+      throw new HttpException('Produto não encontrado', HttpStatus.NOT_FOUND);
+    }
+    return produto;
+  }
+  async findByName(nome: string): Promise<Produto[]> {
+    return await this.produtoRepository.find({
+      where: {
+        nome: ILike(`%${nome}%`),
+      },
+      relations: {
+        usuario: true,
+        categoria: true,
+      },
+    });
+  }
+
+  async create(produto: Produto): Promise<Produto> {
+    if (produto.categoria) {
+      const categoria = await this.categoriaService.findById(
+        produto.categoria.id,
+      );
+      if (!categoria) {
+        throw new HttpException(
+          'Categoria não encontrada',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      const produtoBusca = await this.produtoRepository.findOne({
+        where: {
+          nome: produto.nome,
+        },
+      });
+
+      if (produtoBusca != null) {
+        throw new BadRequestException(`Produto já existe.`);
+      }
+      produto.categoria = categoria;
+    }
+
+    if (produto.usuario) {
+      const usuario = await this.usuarioService.findById(produto.usuario.id);
+
+      if (!usuario) {
+        throw new HttpException(
+          'Usuário não encontrado!',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      produto.usuario = usuario;
+    }
+
+    return await this.produtoRepository.save(produto);
+  }
+  async update(produto: Produto): Promise<Produto> {
+    await this.findById(produto.id);
+
+    if (produto.categoria) {
+      const categoria = await this.categoriaService.findById(
+        produto.categoria.id,
+      );
+
+      if (!categoria) {
+        throw new HttpException(
+          'Categoria não encontrada!',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      produto.categoria = categoria;
+    }
+
+    if (produto.usuario) {
+      const usuario = await this.usuarioService.findById(produto.usuario.id);
+
+      if (!usuario) {
+        throw new HttpException(
+          'Usuário não encontrado!',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      produto.usuario = usuario;
+    }
+
+    return await this.produtoRepository.save(produto);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+
+    return this.produtoRepository.delete(id);
+  }
+}
